e24887d423349e064d5c7e6be0b4c089f1db4414
Luis Alexander <alexander_dx_10@hotmail.com>
2020-12-11 12:35:38 -0500

    Guardando cambios

--- app/disciplinas/Obtener_Disciplinas.php
+++ app/disciplinas/Obtener_Disciplinas.php
@@ -7,7 +7,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers:  Authorization, Origin, X-Requested-With, Content-Type, Accept");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header('content-type: application/json; charset=utf-8');
-
 require './DisciplinaAdo.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
