3dca012ef0ecdf371936d326967a008af21a294a
Luis Lara <alexander_dx_10@hotmail.com>
2020-12-12 16:58:08 -0500

    Guardando cambios

--- view/puntoDeVenta.php
+++ view/puntoDeVenta.php
@@ -143,14 +143,14 @@ if (!isset($_SESSION["IdEmpleado"])) {
                                                                     <th>Quitar</th>
                                                                 </tr>
                                                             </thead>
-                                                            <tbody>
+                                                            <tbody id="tvPlazos">
 
                                                             </tbody>
                                                         </table>
                                                     </div>
                                                     <div class="row">
                                                         <div class="col-md-12">
-                                                            <button class="btn btn-link" type="button">Agregar cuota +</button>
+                                                            <button class="btn btn-link" id="btnPlazos" type="button">Agregar cuota +</button>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -199,7 +199,7 @@ if (!isset($_SESSION["IdEmpleado"])) {
                                                 <th class="sorting" aria-controls="sampleTable" rowspan="1" colspan="1">Acción</th>
                                             </tr>
                                         </thead>
-                                        <tbody id="">
+                                        <tbody id="tbLista">
                                             <!-- tbLista -->
                                             <tr role="row" class="odd">
                                                 <td class="sorting_1">Airi Satou</td>
@@ -260,272 +260,64 @@ if (!isset($_SESSION["IdEmpleado"])) {
         <script>
             let tools = new Tools();
 
-            let tbLista = $("#tbLista");
-            let totalPaginacion = 0;
-            let paginacion = 0;
-            let opcion = 0;
-            let state = false;
-
-            let idClienteUpdate = "";
+            let listaVenta = [];
 
             $(document).ready(function() {
 
-                loadInitClientes();
-
-                $('#modalCobro').on('shown.bs.modal', function() {
-                    // $('#dni').trigger('focus')
-                })
-
                 $("#btnCobrar").click(function() {
-                    $("#modalCobro").modal("show");
-                    // $("#titulo-modal").append('<i class="fa fa-user-plus"></i> Registrar Cliente')
-                    // $('#dni').trigger('focus')
+                    if(listaVenta.length == 0){
+                        tools.AlertWarning("Ventas: ","No hay ítems en la lista para continuar.");
+                    }else{
+                        $("#modalCobro").modal("show");
+                    }                    
                 })
 
                 $("#btnCancelModal").click(function() {
                     //closeClearModal()
                     $("#modalCobro").modal("hide")
-                })
+                });
 
                 $("#btnCloseModal").click(function() {
                     //closeClearModal()
                     $("#modalCobro").modal("hide")
-
-                })
-
-                $("#btnReload").click(function() {
-                    loadInitClientes()
-                })
-
-                $("#btnGuardarModal").click(function() {
-                    if ($("#dni").val() == '' || $("#dni").val().length < 8) {
-                        tools.AlertWarning("Advertencia", "Ingrese un número de dni valido")
-                    } else if ($("#apellidos").val() == '' || $("#apellidos").val().length < 1) {
-                        tools.AlertWarning("Advertencia", "Ingrese un apellido de 2 o mas letras.")
-                    } else if ($("#nombres").val() == '' || $("#nombres").val().length < 1) {
-                        tools.AlertWarning("Advertencia", "Ingrese un nombre de 2 o mas letras.")
-                    } else if ($("#genero").val() == '') {
-                        tools.AlertWarning("Advertencia", "Seleccione su sexo.")
-                    } else if ($("#nacimiento").val() == '') {
-                        tools.AlertWarning("Advertencia", "Ingrese un fecha de nacimiento.")
-                    } else if ($("#celular").val() == '' || $("#celular").val().length < 6) {
-                        tools.AlertWarning("Advertencia", "Ingrese un número de celular valido")
-                    } else {
-                        registrarCliente($("#dni").val(), $("#apellidos").val(), $("#nombres").val(), $(
-                                "#genero").val(),
-                            $("#nacimiento").val(), $("#codigo").val(), $("#email").val(), $("#celular")
-                            .val(),
-                            $("#direccion").val())
-                    }
                 });
 
-                $("#btnAnterior").click(function() {
-                    if (!state) {
-                        if (paginacion > 1) {
-                            paginacion--;
-                            onEventPaginacion();
-                        }
-                    }
-                });
-                $("#btnSiguiente").click(function() {
-                    if (!state) {
-                        if (paginacion < totalPaginacion) {
-                            paginacion++;
-                            onEventPaginacion();
-                        }
-                    }
+                $("#btnPlazos").click(function() {
+                    let currentDate = Date.now();
+                    $("#tvPlazos").append('<tr id="'+currentDate+'">' +
+                        '<td><input class="form-control" type="number" /></td>' +
+                        '<td><input class="form-control" type="date" /></td>' +
+                        '<td><div class="text-center"><input type="checkbox" /></div></td>' +
+                        '<td><button class="btn btn-danger" onclick="removePlazos(\'' +currentDate + '\')"><i class="fa fa-trash"></i></button></td>' +
+                        '</tr>');
+
+                    $("#tvPlazos tr").each(function(row, tr) {        
+                        console.log(row)                
+                        //$(tr).find("td:eq(2)").find("input").val();                            
+                        //console.log($(tr).find("td:eq(2)").find('input[type="checkbox"]').is(':checked'));
+                        // console.log($(tr).find("td:eq(2)").find("input").val());
+                    });
                 });
 
-                $("#txtSearch").keypress(function() {
-                    if ($("#txtSearch").val().trim() != '') {
-                        loadTableClientes($("#txtSearch").val().trim());
-                    }
+                $("#btnGuardarModal").click(function(){
+                    registrarVenta();
                 });
 
             });
 
-            function loadInitClientes() {
-                if (!state) {
-                    paginacion = 1;
-                    loadTableClientes("");
-                    opcion = 0;
+            function registrarVenta(){             
+                if($("#pills-home-tab").hasClass("active")){
+                   if(!tools.isNumeric($("#monto").val())){
+                    tools.AlertWarning("Venta: ","Ingrese el monto por favor.");
+                    $("#monto").focus();
+                   }
+                }else{
+
                 }
             }
 
-            function onEventPaginacion() {
-                switch (opcion) {
-                    case 0:
-                        loadTableClientes("");
-                        break;
-                }
-            }
-
-            function loadTableClientes(buscar) {
-                $.ajax({
-                    url: "../app/cliente/Obtener_Clientes.php",
-                    method: "",
-                    data: {
-                        opcion: 2,
-                        page: paginacion,
-                        datos: buscar
-                    },
-                    beforeSend: function() {
-                        state = true;
-                        tbLista.empty();
-                        tbLista.append(
-                            '<tr role="row" class="odd"><td class="sorting_1" colspan="8" style="text-align:center"><img src="./images/loading.gif" width="100"/><p>cargando información...</p></td></tr>'
-                        );
-                    },
-                    success: function(result) {
-                        let data = JSON.parse(result);
-                        if (data.estado == 1) {
-                            tbLista.empty();
-                            for (let cliente of data.clientes) {
-
-                                let btnUpdate =
-                                    '<button class="btn btn-warning btn-sm" onclick="loadUpdateCliente(\'' +
-                                    cliente.idCliente + '\')">' +
-                                    '<i class="fa fa-wrench"></i> Editar' +
-                                    '</button>';
-
-                                tbLista.append('<tr role="row" class="odd">' +
-                                    '<td class="sorting_1">' + cliente.id + '</td>' +
-                                    '<td>' + cliente.dni + '</td>' +
-                                    '<td>' + cliente.apellidos + " " + cliente.nombres + '</td>' +
-                                    '<td>' + cliente.celular + '</td>' +
-                                    '<td>' + cliente.email + '</td>' +
-                                    '<td>' + cliente.direccion + '</td>' +
-                                    '<td>' + (cliente.membresia == 1 ? cliente.membresia + " MEMBRESIA(S)" :
-                                        "NINGUNA") + "<br>" + (cliente.venta == 1 ? cliente.venta +
-                                        " deuda(s)" : "0 deudas") + '</td>' +
-                                    '<td>' + btnUpdate + '</td>' +
-                                    '</tr>');
-                            }
-                            totalPaginacion = parseInt(Math.ceil((parseFloat(data.total) / parseInt(
-                                10))));
-                            $("#lblPaginaActual").html(paginacion);
-                            $("#lblPaginaSiguiente").html(totalPaginacion);
-                            state = false;
-                        } else {
-                            tbLista.empty();
-                            tbLista.append(
-                                '<tr role="row" class="odd"><td class="sorting_1" colspan="8" style="text-align:center"><p>' +
-                                data.mensaje + '</p></td></tr>');
-                            state = false;
-                        }
-                    },
-                    error: function(error) {
-                        tbLista.empty();
-                        tbLista.append(
-                            '<tr role="row" class="odd"><td class="sorting_1" colspan="8" style="text-align:center"><p>' +
-                            error.responseText + '</p></td></tr>');
-                        state = false;
-                    }
-                });
-            }
-
-            function registrarCliente(dni, apellidos, nombres, genero, nacimiento, codigo, email, celular, direccion) {
-                $.ajax({
-                    url: "../app/cliente/Crud_Clientes.php",
-                    method: "POST",
-                    accepts: "application/json",
-                    contentType: "application/json",
-                    data: JSON.stringify({
-                        "idCliente": idClienteUpdate,
-                        "dni": dni,
-                        "apellidos": (apellidos.toUpperCase()).trim(),
-                        "nombres": (nombres.toUpperCase()).trim(),
-                        "sexo": genero,
-                        "fechaNacimiento": nacimiento,
-                        "codigo": (codigo.toUpperCase()).trim(),
-                        "email": email.trim(),
-                        "celular": celular,
-                        "direccion": (direccion.toUpperCase()).trim()
-                    }),
-                    beforeSend: function() {
-                        $("#btnGuardarModal").empty();
-                        $("#btnGuardarModal").append('<img src="./images/loading.gif" width="25" height="25" />')
-                    },
-                    success: function(result) {
-                        if (result.estado == 1) {
-                            tools.AlertSuccess("Mensaje", result.mensaje);
-                            loadInitClientes();
-                            $("#btnGuardarModal").empty();
-                            $("#btnGuardarModal").append('<i class="fa fa-save"></i> Guardar');
-                            closeClearModal();
-                        } else {
-                            tools.AlertWarning("Mensaje", result.mensaje);
-                            $("#btnGuardarModal").empty();
-                            $("#btnGuardarModal").append('<i class="fa fa-save"></i> Guardar');
-                        }
-                    },
-                    error: function(error) {
-                        tools.AlertError("Error", error.responseText);
-                        $("#btnGuardarModal").empty();
-                        $("#btnGuardarModal").append('<i class="fa fa-save"></i> Guardar')
-                    }
-                });
-            }
-
-            function closeClearModal() {
-                $("#modalCliente").modal("hide")
-                $("#titulo-modal").empty()
-                $("#btnAccion").empty()
-
-                $("#dni").val("")
-                $("#apellidos").val("")
-                $("#nombres").val("")
-                $("#genero").val("1")
-                $("#nacimiento").val("")
-                $("#codigo").val("")
-                $("#email").val("")
-                $("#celular").val("")
-                $("#direccion").val("")
-                idClienteUpdate = "";
-            }
-
-            function loadUpdateCliente(idCliente) {
-                $("#modalCliente").modal("show");
-                $("#titulo-modal").append('<i class="fa fa-user"></i> Editar Cliente');
-                idClienteUpdate = idCliente;
-                $.ajax({
-                    url: "../app/cliente/Obtener_Clientes_By_Id.php",
-                    method: "POST",
-                    accepts: "application/json",
-                    contentType: "application/json",
-                    data: JSON.stringify({
-                        "idCliente": idCliente
-                    }),
-                    beforeSend: function() {
-                        // $("#btnGuardarModal").empty();
-                        // $("#btnGuardarModal").append('<img src="./images/loading.gif" width="25" height="25" />')
-                    },
-                    success: function(result) {
-                        if (result.estado == 1) {
-                            let cliente = result.cliente;
-                            $("#dni").val(cliente.dni)
-                            $("#apellidos").val(cliente.apellidos)
-                            $("#nombres").val(cliente.nombres)
-                            $("#genero").val(cliente.sexo)
-                            document.getElementById("nacimiento").value = tools.getDateForma(cliente
-                                .fechaNacimiento, 'yyyy-mm-dd')
-                            $("#codigo").val(cliente.codigo)
-                            $("#email").val(cliente.email)
-                            $("#celular").val(cliente.celular)
-                            $("#direccion").val(cliente.direccion)
-
-                        } else {
-                            tools.AlertWarning("Mensaje", result.mensaje)
-                            $("#btnGuardarModal").empty();
-                            $("#btnGuardarModal").append('<i class="fa fa-save"></i> Guardar')
-                        }
-                    },
-                    error: function(error) {
-                        tools.AlertError("Error", error.responseText);
-                        $("#btnGuardarModal").empty();
-                        $("#btnGuardarModal").append('<img src="./images/loading.gif" width="25" height="25" />')
-                    }
-                });
+            function removePlazos(idPlazos){
+                $("#" + idPlazos).remove();
             }
         </script>
     </body>
